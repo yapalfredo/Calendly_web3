8f8a48f8dcac70db39822707dcc25a818816f507
al <alfredo.yap@stu.bmcc.cuny.edu>
2022-03-04 23:05:25 -0500

    started with testing part

--- contracts/Calend3.sol
+++ contracts/Calend3.sol
@@ -3,12 +3,18 @@ pragma solidity ^0.8.0;
 
 contract Calend3 {
     uint rate;
+    address owner; 
+
+    constructor(){
+        owner = msg.sender;
+    }
 
     function getRate() public view returns (uint){
         return rate; 
     }
 
     function setRate(uint _rate) public {
+        require(msg.sender == owner, "Only the owner can set the rate");
         rate = _rate;
     }
 }
