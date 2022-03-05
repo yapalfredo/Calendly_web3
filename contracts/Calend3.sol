34f750f406d869c8a5a0f6e8e008ffd9f0ad3d51
al <alfredo.yap@stu.bmcc.cuny.edu>
2022-03-04 21:43:10 -0500

    added getter setter for rate

--- contracts/Calend3.sol
+++ contracts/Calend3.sol
@@ -1,5 +1,14 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract Calend3 {
-    
+    uint rate;
+
+    function getRate() public view returns (uint){
+        return rate; 
+    }
+
+    function setRate(uint _rate) public {
+        rate = _rate;
+    }
 }
