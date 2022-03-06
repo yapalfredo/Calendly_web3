f79e513c8fe013bef4a390f4b9d7468896542446
al <alfredo.yap@stu.bmcc.cuny.edu>
2022-03-06 13:25:44 -0500

    started enditing frontend file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Calend3 - The Web3 variant of Calendly</h1>
+        <div id="slogan">Web3 Appointment Scheduling App</div>
+
+        <button>Connect Wallet</button>
       </header>
     </div>
   );
