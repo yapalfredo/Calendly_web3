a8d1aa792869417dc4629d4b61c0df3a8ca6cab6
al <alfredo.yap@stu.bmcc.cuny.edu>
2022-03-09 13:35:51 -0500

    almost done with twilio and alchemy notify integration

--- webhooks/index.js
+++ webhooks/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { response } = require("express");
+const { Client } = require("twilio/lib/twiml/VoiceResponse");
+const dotenv = require("dotenv").config();
 
 const app = express();
 const PORT = 3100;
@@ -8,10 +11,32 @@ const PORT = 3100;
 app.use(bodyParser.json());
 
 //start the server on port 3100
-app.listen(POST, () => console.log(`Running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+
+// set up twilio client
+const client = require("twilio")(
+  process.env.ACCOUNT_SID,
+  process.env.AUTH_TOKEN
+);
 
 //process a GET request to http://localhost:3100/hello
-app.get("/hello",(request, response) =>{
-    console.log(request.body);
-    response.send("HI!");
+app.get("/hello", (request, response) => {
+  console.log(request.body);
+  response.send("HI!");
+});
+
+app.post("/webhook", (request, response) => {
+  console.log(request.body);
+  const activity = request.body.activity;
+  const message = `💰💰💰 ${activity[0].fromAddress} paid you ${activity[0].value} ETH. https://goerli.etherscan.io/tx/${activity[0].hash} 💰💰💰`;
+
+  client.messages
+    .create({
+      body: message,
+      from: process.env.FROM_PHONE,
+      to: process.env.TO_PHONE,
+    })
+    .then((message) => console.log(message.sid));
+
+  response.status(200).end;
 });
